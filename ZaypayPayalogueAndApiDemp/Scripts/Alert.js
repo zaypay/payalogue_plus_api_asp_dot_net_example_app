628722c8fd46fb7c1bdd73df775f64778b44be79
ashahzad4 <ashahzad4@gmail.com>
2012-11-22 16:00:30 +0100

    DELETED ALL

--- ZaypayPayalogueAndApiDemp/Scripts/Alert.js
+++
@@ -1,9 +0,0 @@
-﻿
-function ShowError(message) {    
-    $("#alert-box").html("<div id='error-mesg' class='alert alert-error fade in' style='width:500px;'><h4 class='alert-heading'>" + message + "</h4></div>");
-    CloseAlert();
-}
-
-function CloseAlert() {
-    window.setTimeout(function () { $("#error-mesg").alert('close') }, 2000);
-}
